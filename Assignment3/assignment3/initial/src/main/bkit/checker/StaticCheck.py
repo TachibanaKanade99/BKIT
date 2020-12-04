29824e6392458527dbfd4e9d179532eace9451bc
TachibanaKanade99 <sad361975@gmail.com>
2020-12-04 15:32:44 +0700

    update ass3

--- Assignment3/assignment3/initial/src/main/bkit/checker/StaticCheck.py
+++ Assignment3/assignment3/initial/src/main/bkit/checker/StaticCheck.py
@@ -1,3 +1,4 @@
+# ID: 1752595
 
 """
  * @author nhphung
@@ -44,6 +45,15 @@ class Symbol:
     name: str
     mtype:Type
 
+# Operand:
+class Operand():
+    def __init__(self, opName=None, opType=None, opKind=None, param_lst=None):
+        self.opName = opName
+        self.opType = opType
+        self.opKind = opKind
+        self.param_lst = param_lst # use for function only:
+
+
 class StaticChecker(BaseVisitor):
     def __init__(self,ast):
         self.ast = ast
@@ -64,8 +74,193 @@ class StaticChecker(BaseVisitor):
     def check(self):
         return self.visit(self.ast,self.global_envi)
 
-    def visitProgram(self,ast, c):
-        [self.visit(x,c) for x in ast.decl]
+    def visitProgram(self,ast, param):
+        decl_lst = [[]]
+
+        for x in ast.decl:
+            decl = x.accept(self, decl_lst)
+            decl_lst[0].append(decl)
+
+        # check only:
+        for x in decl_lst[0]:
+            print(x.opName, " ", x.opType)
+
+    def visitVarDecl(self, ast, param):
+        decl_lst = param
+        var_name = ast.variable.accept(self, decl_lst+["vardecl"])
+        # var_dimension = ast.varDimen.accept(self, decl_lst)
+
+        # Check redeclared variable:
+        for decl in decl_lst[0]:
+            if var_name == decl.opName:
+
+                if param[-1] is not None:
+                    if param[-1] == 'param':
+                        raise Redeclared(Parameter(), var_name)
+                
+                raise Redeclared(Variable(), var_name)
+        
+        # Infer type for variable:
+        if ast.varInit is not None:
+            var_value = ast.varInit.accept(self, decl_lst)
+            var_type = var_value.opType
+        else:
+            var_type = None
+
+        return Operand(var_name, var_type, "variable", None)
+
+    def visitFuncDecl(self, ast, param):
+        decl_lst = param
+        func_name = ast.name.accept(self, decl_lst+["funcdecl"])
+        func_type = None
+        param_lst = []
+
+        # Check redeclared function
+        for decl in decl_lst[0]:
+            if func_name == decl.opName:
+                raise Redeclared(Function(), func_name)
+
+        # Add function name to global scope:
+        # Use new list:
+        new_decl_lst = decl_lst.copy()
+        new_decl_lst[0].append(Operand(func_name, func_type, "function", param_lst))
+
+        # visit param:
+        for x in ast.param:
+            param_decl = x.accept(self, [param_lst, "param"])
+            param_lst.append(param_decl)
+
+        # visit body[0]:
+        # copy a new_lst
+        local_lst = param_lst.copy()
+
+        for y in ast.body[0]:
+            decl = y.accept(self, [local_lst])
+            local_lst.append(decl)
+
+        # visit body[1]:
+        new_decl_lst = [local_lst] + new_decl_lst
+
+        for z in ast.body[1]:
+            stmt = z.accept(self, new_decl_lst)
+
+        return Operand(func_name, func_type, "function", param_lst)
+
+    def visitBinaryOp(self, ast, param):
+        decl_lst = param
+        op = ast.op
+        left_expr = ast.left.accept(self, decl_lst)
+        right_expr = ast.right.accept(self, decl_lst)
+
+        if left_expr is None and right_expr is not None:
+            left_expr.opType = right_expr.opType
+        if left_expr is not None and right_expr is None:
+            right_expr.opType = left_expr.opType
+
+        if op == '+' or op == '-' or op == '*' or op == '\\' or op == '%':
+            if left_expr.opType is not int or right_expr.opType is not int:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Operand(None, int, "expression", None)
+
+        if op == '+.' or op == '-.' or op == '*.' or op == '\.':
+            if left_expr.opType is not float or right_expr.opType is not float:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Operand(None, float, "expression", None)
+
+        if op == '!' or op == '&&' or op == '||':
+            if left_expr.opType is not bool or right_expr.opType is not bool:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Operand(None, bool, "expression", None)
+
+        if op == '==' or op == '!=' or op == '>' or op == '<' or op == '>=' or op == '<=':
+            if left_expr.opType is not int or right_expr.opType is not int:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Operand(None, int, "expression", None)
+
+        if op == '=/=' or op == '<.' or op == '>.' or op == '<=.' or op == '>=.':
+            if left_expr.opType is not float or right_expr.opType is not float:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Operand(None, float, "expression", None)
+
+    def visitUnaryOp(self, ast, param):
+        decl_lst = param
+        op = ast.op
+        expr = ast.body.accept(self, decl_lst)
+
+        
+    
+    def visitCallExpr(self, ast, param):
+        decl_lst = param
+        func_name = ast.method.accept(self, decl_lst+["func_call"])
+
+    def visitAssign(self, ast, param):
+        decl_lst = param
+        lhs = ast.lhs.accept(self, decl_lst)
+        rhs = ast.rhs.accept(self, decl_lst)
+
+        # Check type of lhs and rhs:
+        if lhs.opType is None and rhs.opType is None:
+            raise TypeCannotBeInferred(ast)
+        elif lhs.opType is None and rhs.opType is not None:
+            lhs.opType = rhs.opType
+        elif lhs.opType is not None and rhs.opType is None:
+            rhs.opType = lhs.opType
+        elif lhs.opType != rhs.opType:
+            raise TypeMismatchInStatement(ast)
+
+    def visitId(self, ast, param):
+        decl_lst = param
+        flatten_decl_lst = []
+        isFunc = None
+
+        # Use for decl:
+        if decl_lst[-1] == "vardecl" or decl_lst[-1] == "funcdecl":
+            return ast.name
+
+        # Check if id is function name:
+        if decl_lst[-1] == "func_call":
+            isFunc = decl_lst.pop()
+        
+        for sublst in decl_lst:
+            for x in sublst:
+                flatten_decl_lst.append(x)
+
+        for decl in flatten_decl_lst:
+            if ast.name == decl.opName:
+                return decl
+
+        if isFunc == "func_call":
+            raise Undeclared(Function(), ast.name)
+        else:
+            raise Undeclared(Identifier(), ast.name)
+
+    # Literal:
+    def visitIntLiteral(self, ast, param):
+        return Operand(None, int, "literal", None)
+    
+    def visitFloatLiteral(self, ast, param):
+        return Operand(None, float, "literal", None)
+    
+    def visitBooleanLiteral(self, ast, param):
+        return Operand(None, bool, "literal", None)
+    
+    def visitStringLiteral(self, ast, param):
+        return Operand(None, str, "literal", None)
+
+    def visitArrayLiteral(self, ast, param):
+        decl_lst = param
+        lit_lst = []
+        for x in ast.value:
+            lit = x.accept(self, decl_lst)
+            lit_lst.append(lit)
+        
+        return Operand(None, lit_lst)
+    
 
 
 
--- Assignment3/assignment3/initial/src/test/CheckSuite.py
+++ Assignment3/assignment3/initial/src/test/CheckSuite.py
@@ -5,54 +5,196 @@ from AST import *
 
 class CheckSuite(unittest.TestCase):
 
-    def test_undeclared_function(self):
-        """Simple program: main"""
-        input = """Function: main
-                   Body: 
-                        foo();
-                   EndBody."""
-        expect = str(Undeclared(Function(),"foo"))
-        self.assertTrue(TestChecker.test(input,expect,400))
+    # def test_undeclared_function(self):
+    #     """Simple program: main"""
+    #     input = """Function: main
+    #                Body: 
+    #                     foo();
+    #                EndBody."""
+    #     expect = str(Undeclared(Function(),"foo"))
+    #     self.assertTrue(TestChecker.test(input,expect,400))
 
-    def test_diff_numofparam_stmt(self):
-        """Complex program"""
-        input = """Function: main  
-                   Body:
-                        printStrLn();
-                    EndBody."""
-        expect = str(TypeMismatchInStatement(CallStmt(Id("printStrLn"),[])))
-        self.assertTrue(TestChecker.test(input,expect,401))
+    # def test_diff_numofparam_stmt(self):
+    #     """Complex program"""
+    #     input = """Function: main  
+    #                Body:
+    #                     printStrLn();
+    #                 EndBody."""
+    #     expect = str(TypeMismatchInStatement(CallStmt(Id("printStrLn"),[])))
+    #     self.assertTrue(TestChecker.test(input,expect,401))
     
-    def test_diff_numofparam_expr(self):
-        """More complex program"""
-        input = """Function: main 
-                    Body:
-                        printStrLn(read(4));
-                    EndBody."""
-        expect = str(TypeMismatchInExpression(CallExpr(Id("read"),[IntLiteral(4)])))
-        self.assertTrue(TestChecker.test(input,expect,402))
+    # def test_diff_numofparam_expr(self):
+    #     """More complex program"""
+    #     input = """Function: main 
+    #                 Body:
+    #                     printStrLn(read(4));
+    #                 EndBody."""
+    #     expect = str(TypeMismatchInExpression(CallExpr(Id("read"),[IntLiteral(4)])))
+    #     self.assertTrue(TestChecker.test(input,expect,402))
 
-    def test_undeclared_function_use_ast(self):
-        """Simple program: main """
-        input = Program([FuncDecl(Id("main"),[],([],[
-            CallExpr(Id("foo"),[])]))])
-        expect = str(Undeclared(Function(),"foo"))
-        self.assertTrue(TestChecker.test(input,expect,403))
+    # def test_undeclared_function_use_ast(self):
+    #     """Simple program: main """
+    #     input = Program([FuncDecl(Id("main"),[],([],[
+    #         CallExpr(Id("foo"),[])]))])
+    #     expect = str(Undeclared(Function(),"foo"))
+    #     self.assertTrue(TestChecker.test(input,expect,401))
 
-    def test_diff_numofparam_expr_use_ast(self):
-        """More complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],([],[
-                    CallStmt(Id("printStrLn"),[
-                        CallExpr(Id("read"),[IntLiteral(4)])
-                        ])]))])
-        expect = str(TypeMismatchInExpression(CallExpr(Id("read"),[IntLiteral(4)])))
-        self.assertTrue(TestChecker.test(input,expect,404))
+    # def test_diff_numofparam_expr_use_ast(self):
+    #     """More complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],([],[
+    #                 CallStmt(Id("printStrLn"),[
+    #                     CallExpr(Id("read"),[IntLiteral(4)])
+    #                     ])]))])
+    #     expect = str(TypeMismatchInExpression(CallExpr(Id("read"),[IntLiteral(4)])))
+    #     self.assertTrue(TestChecker.test(input,expect,402))
 
-    def test_diff_numofparam_stmt_use_ast(self):
-        """Complex program"""
-        input = Program([
-                FuncDecl(Id("main"),[],([],[
-                    CallStmt(Id("printStrLn"),[])]))])
-        expect = str(TypeMismatchInStatement(CallStmt(Id("printStrLn"),[])))
-        self.assertTrue(TestChecker.test(input,expect,405))
+    # def test_diff_numofparam_stmt_use_ast(self):
+    #     """Complex program"""
+    #     input = Program([
+    #             FuncDecl(Id("main"),[],([],[
+    #                 CallStmt(Id("printStrLn"),[])]))])
+    #     expect = str(TypeMismatchInStatement(CallStmt(Id("printStrLn"),[])))
+    #     self.assertTrue(TestChecker.test(input,expect,403))
+
+    def test_redeclared_global_var(self):
+        input = """
+        Var: a;
+        Var: a = 1;        
+        """
+        expect = str(Redeclared(Variable(), "a"))
+        self.assertTrue(TestChecker.test(input, expect, 401))
+
+    def test_redeclared_many_global_var(self):
+        input = """
+        Var: b = {1, 2, 3};
+        Var: a;
+        Var: b = 1;        
+        """
+        expect = str(Redeclared(Variable(), "b"))
+        self.assertTrue(TestChecker.test(input, expect, 402))
+
+    def test_redeclared_func(self):
+        input = """
+        Var: foo = 1.2;
+        Function: foo
+            Body:
+            EndBody.
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Redeclared(Function(), "foo"))
+        self.assertTrue(TestChecker.test(input, expect, 403))
+
+    def test_redeclared_param(self):
+        input = """
+        Function: foo
+            Parameter: a, b[10], a
+            Body:
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Redeclared(Parameter(), "a"))
+        self.assertTrue(TestChecker.test(input, expect, 404))
+
+    def test_redeclared_param_is_array_lit(self):
+        input = """
+        Function: foo
+            Parameter: a
+            Body:
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+
+        Function: foo2
+            Parameter: b[10], b[1]
+            Body:
+            EndBody.
+        """
+        expect = str(Redeclared(Parameter(), "b"))
+        self.assertTrue(TestChecker.test(input, expect, 404))
+
+    def test_redeclared_var_in_func_body(self):
+        input = """
+        Function: foo
+            Parameter: a
+            Body:
+                Var: a = 1;
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Redeclared(Variable(), "a"))
+        self.assertTrue(TestChecker.test(input, expect, 405))
+
+    def test_var_declared_in_func_body(self):
+        input = """
+        Var: a;
+        Function: foo
+            Parameter: b
+            Body:
+                Var: a = 1;
+                Var: b = {1, 2, 3};
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Redeclared(Variable(), "b"))
+        self.assertTrue(TestChecker.test(input, expect, 406))
+
+    def test_undeclared_identifier(self):
+        input = """
+        Function: foo
+            Body:
+                a = 1;
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Undeclared(Identifier(), "a"))
+        self.assertTrue(TestChecker.test(input, expect, 407))
+
+    def test_undeclared_function(self):
+        input = """
+        Function: foo
+            Parameter: a
+            Body:
+                a = foo1();
+            EndBody.
+
+        Function: main
+            Body:
+            EndBody.
+        """
+        expect = str(Undeclared(Function(), "foo1"))
+        self.assertTrue(TestChecker.test(input, expect, 408))
+
+    def test_simple_bin_op(self):
+        input = """
+        Function: main
+            Body:
+                Var: a, x = 1.2;
+                a = 1 + x;
+            EndBody.
+        """
+        expect = str(TypeMismatchInExpression(BinaryOp("+", IntLiteral(1), Id("x"))))
+        self.assertTrue(TestChecker.test(input, expect, 409))
+
+    
+
+
+
+
+
